1c6f179aee7d57d686d51ee18116c8565e38c681
adhasan7 <adsansapu@gmail.com>
2023-11-02 13:05:44 +0700

    fitur nambah json

---
+++ latihan1.js
@@ -0,0 +1,15 @@
+const mahasiswa = [
+  {
+    nama: "Ziah",
+    nrp: "0188299",
+    email: "ziah@gmail.com",
+  },
+  {
+    nama: "Zim",
+    nrp: "018829dd9",
+    email: "zim@gmail.com",
+  },
+];
+
+const data = JSON.stringify(mahasiswa);
+console.log(data);
